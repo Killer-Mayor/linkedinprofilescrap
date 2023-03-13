aec53294c6322a13ff4a897cb354cd46cffc1ee0
Shrey Anish G <shreyanish@shrey.local>
2023-03-13 21:41:47 +0530

    search function enabled

--- findLinkedin/findLinkedin.py
+++ findLinkedin/findLinkedin.py
@@ -10,38 +10,45 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 import time
 
-ser = Service("/Users/shreyanish/Dev/pythonXmentorrelations/findLinkedin/chromedriver.exe")
-op = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=ser, options=op)
 
-driver.get("https://linkedin.com")
-
-username_input = driver.find_element("id", "session_key")
-username_input.send_keys("storieape420@gmail.com")
-username_input.submit()
-
-password_input = driver.find_element("id", "session_password")
-password_input.send_keys("StorieApe@19818525")
-password_input.submit()
-
-signin_button = driver.find_element("type", "submit")  
-signin_button.click()
-
-
-'''try:
-    search = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS,  "search-global-typeahead__input"))
-    )
-    search.send_keys("shrey anish")
-    search.submit()
-
-except:
-    driver.quit()'''
-
-
+#connecting google sheets
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("findLinkedin/creds.json", scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("pytest").sheet1
+
+
+#initianting web driver
+ser = Service("/Users/shreyanish/Dev/pythonXmentorrelations/findLinkedin/chromedriver.exe")
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)
+
+#signing into linkedin
+
+def signin_linkedin():
+    driver.get("https://www.linkedin.com/")
+
+    username_input = driver.find_element("id", "session_key")
+    username_input.send_keys("storieape420@gmail.com")
+    username_input.submit()
+    time.sleep(1)
+
+    password_input = driver.find_element("id", "session_password")
+    password_input.send_keys("StorieApe@19818525")
+    password_input.submit()
+    time.sleep(1)
+
+
+def search_profile():
+    search = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "search-global-typeahead__input"))
+    )
+    search.send_keys("kunal shah cred")
+    search.send_keys(Keys.ENTER)
+    time.sleep(30)
+
+
+signin_linkedin()
+search_profile()
