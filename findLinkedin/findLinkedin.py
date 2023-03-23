f7edf38213d03c388591d75f0471473b62d65047
Shrey Anish G <shreyanish@shrey.local>
2023-03-23 10:19:28 +0530

    final commit

--- findLinkedin/findLinkedin.py
+++ findLinkedin/findLinkedin.py
@@ -20,7 +20,7 @@ scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/aut
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("findLinkedin/creds.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open("pytest").sheet1
+sheet = client.open("pytest").sheet1 
 
 #initianting web driver
 ser = Service("/Users/shreyanish/Dev/pythonXmentorrelations/findLinkedin/chromedriver.exe")
@@ -30,65 +30,50 @@ wait = WebDriverWait(driver, 30)
 
 #signing into linkedin
 
-def signin_linkedin():
-    driver.get("https://www.linkedin.com/")
-
-    username_input = driver.find_element("id", "session_key")
-    username_input.send_keys("storieape420@gmail.com")
-    username_input.submit()
-    time.sleep(1)
-
-    password_input = driver.find_element("id", "session_password")
-    password_input.send_keys("StorieApe@19818525")
-    password_input.submit()
-    time.sleep(10)
-
 def search_profile(name, company, i):
 
     search_term = "site:linkedin.com/in/ and {Name} {Company} people".format(Name = name, Company = company)
+    print(search_term)
 
     search_bar = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input'))
     )
+
     search_bar.clear()
     search_bar.send_keys(search_term)
-    search_bar.send_keys(Keys.ENTER)
+    search_bar.send_keys(Keys.ENTER) 
+    time.sleep(2)
     
-
-    print(search_term)
-    
-    
-    profile_div = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="rso"]/div[1]/div/div/div[1]/div/a'))
+    profile_div = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input'))
     )
+    time.sleep(5)
     profile_url = profile_div.get_attribute('href')
-    pprint(profile_url)
-
+    print(profile_url)
+    
     sheet.update_cell(i,5,profile_url)
     
-    
 
-with open("findLinkedin/cxosfinal copy.csv", "r") as file:
+with open("findLinkedin/pytest - Sheet2 (1).csv", "r") as file:
     csvreader = csv.DictReader(file)
 
     #signin_linkedin()
 
     driver.get("https://www.google.com/search?q=conquest+bits+pilani&oq=conquest+bits+pilani&aqs=chrome..69i57j0i390.4657j0j7&sourceid=chrome&ie=UTF-8")
-    rowcount = 2
+    rowcount = 230
     
     for row in csvreader:
-        name = row['Name']
-        print(name)
-        company = row['Company']
-        print(company)
+        Name = row['Name']
+        print(Name)
+        Company = row['Company']
+        print(Company)
         time.sleep(5)
         try:
-            search_profile(name = name, company = company, i=rowcount)
+            search_profile(name = Name, company = Company, i=rowcount)
         except:
             continue
-
-        rowcount = rowcount + 1
         
+        rowcount += 1
     
 
     
